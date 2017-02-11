aeb7943dfcf10154258940277c225b1c4abc69e6
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-11 15:19:28 -0600

    Added build definition list to project details page

---
+++ WhoBrokeIt/Controls/BuildDefinition.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Messier16.VstsClient.Objects;
+using Xamarin.Forms;
+
+namespace WhoBrokeIt.UI.Controls
+{
+	public partial class BuildDefinition : ContentView
+	{
+		public BuildDefinition()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
+			var build = BindingContext as BasicBuild;
+			if (build != null)
+			{
+				BuildNameLabel.Text = build.Name;
+				RequestorNameLabel.Text = build.AuthoredBy.DisplayName;
+			}	
+		}
+	}
+}
--- WhoBrokeIt/Views/ProjectDetailsPage.xaml.cs
+++ WhoBrokeIt/Views/ProjectDetailsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WhoBrokeIt.UI.Controls;
 using Xamarin.Forms;
 
 namespace WhoBrokeIt.UI.Views
@@ -20,9 +20,26 @@ namespace WhoBrokeIt.UI.Views
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            var project = await WhoBrokeItApp.RealCurrent.Client.GetProject(_projectId);
+			var client = WhoBrokeItApp.RealCurrent.Client;
+            var project = await client.GetProject(_projectId);
             Title = project.Name;
             DescriptionLabel.Text = project.Description;
+
+			var definitions = await client.BuildsForProject(_projectId);
+			if (definitions.Count > 0)
+			{
+				foreach (var def in definitions.Value)
+				{
+					var buildDefView = new BuildDefinition();
+					buildDefView.BindingContext = def;
+					BuildDefsStack.Children.Add(buildDefView);
+				}
+			}
+			else 
+			{
+				BuildDefsStack.Children.Add(
+					new Image { Source = "build_definition" });
+			}
         }
     }
 }
