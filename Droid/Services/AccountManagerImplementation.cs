aca76edcf4e7b4f7c7b71c374ad17837199597ed
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-10 23:05:54 -0600

    Added Xamarin Auth & implementations

---
+++ Droid/Services/AccountManagerImplementation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using WhoBrokeIt.UI.Services;
+using Xamarin.Auth;
+
+namespace WhoBrokeIt.Droid.Services
+{
+	public class AccountManagerImplementation : IAccountManager
+	{
+		public AccountManagerImplementation()
+		{
+		}
+
+		public void EraseAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetTokenForInstance(string instance)
+		{
+			var accounts = AccountStore.Create()
+									   .FindAccountsForService("visualstudio");
+
+			return accounts
+				.FirstOrDefault(
+					(arg) => arg.Username.Equals(instance))?.Properties["token"];
+		}
+
+		public void SaveTokenForInstance(string instance, string token)
+		{
+			var account = new Account(instance,
+			new Dictionary<string, string>
+			{
+				{"token", token}
+			});
+			AccountStore.Create().Save(account, "visualstudio");
+		}
+
+	}
+}
---
+++ WhoBrokeIt/Services/IAccountManager.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WhoBrokeIt.UI.Services
+{
+	public interface IAccountManager
+	{
+		void SaveTokenForInstance(string instance, string token);
+		
+		string GetTokenForInstance(string instance);
+
+		void EraseAll();
+	}
+}
