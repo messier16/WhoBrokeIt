ffe088cab60b0d2aa6ad1ea33f07ce348d80fc1b
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-13 12:24:53 -0600

    Edit buttons

---
+++ WhoBrokeIt/Controls/Cells/CommitCell.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Messier16.VstsClient.Objects;
+using Xamarin.Forms;
+
+namespace WhoBrokeIt.UI.Controls.Cells
+{
+	public partial class CommitCell : ViewCell
+	{
+		public CommitCell()
+		{
+			InitializeComponent();
+
+		}
+
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
+			var commit = BindingContext as Commit;
+			if (commit != null)
+			{
+				CommitAuthorLabel.Text = commit.Author.Name;
+				CommitDescriptionLabel.Text = commit.Comment;
+				EditionsLabel.Text = commit.ChangeCounts.Edit?.ToString() ?? "-";
+				AdditionsLabel.Text = commit.ChangeCounts.Add?.ToString() ?? "-";
+				DeletionsLabel.Text = commit.ChangeCounts.Delete?.ToString() ?? "-";
+			}
+		}
+	}
+}
