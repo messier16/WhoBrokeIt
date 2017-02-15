66e26e0404f5bb2aaf7c86ce17c4c8bc15d424f3
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-14 19:32:31 -0600

    UI improvements

---
+++ WhoBrokeIt/Controls/Cells/BuildCell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Messier16.VstsClient.Objects;
+using Xamarin.Forms;
+
+namespace WhoBrokeIt.UI.Controls.Cells
+{
+	public partial class BuildCell : ViewCell
+	{
+		public BuildCell()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
+			var build = BindingContext as Build;
+			if (build != null)
+			{
+				BuildStatusLabel.Text = build.Status;
+				BuildElapsedTimeLabel.Text = build.StartTime.LocalDateTime + " - " + build.FinishTime.LocalDateTime;
+			}
+		}
+	}
+}
