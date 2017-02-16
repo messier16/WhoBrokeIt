2bb1c553af02405a7bbebc128b332ff6fa2ae286
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-16 08:30:36 -0600

    Branded cell for iOS

---
+++ iOS/Controls/Cells/BrandCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace WhoBrokeIt.iOS.Controls.Cells
+{
+	public partial class BrandCell : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString("BrandCell");
+		public static readonly UINib Nib;
+
+		static BrandCell()
+		{
+			Nib = UINib.FromName("BrandCell", NSBundle.MainBundle);
+		}
+
+		protected BrandCell(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+	}
+}
---
+++ iOS/Controls/Cells/BrandCellRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WhoBrokeIt.iOS.Controls.Cells;
+using FormsBrandedCell = WhoBrokeIt.UI.Controls.Cells.BrandCell;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(FormsBrandedCell), typeof(BrandCellRenderer))]
+namespace WhoBrokeIt.iOS.Controls.Cells
+{
+	public class BrandCellRenderer : ViewCellRenderer
+	{
+		public override UIKit.UITableViewCell GetCell(Xamarin.Forms.Cell item, UIKit.UITableViewCell reusableCell, UIKit.UITableView tv)
+		{
+			var tvc = tv.DequeueReusableCell("BrandCell");
+			if (tvc == null)
+			{
+				tv.RegisterNibForCellReuse(BrandCell.Nib, "BrandCell");
+				tvc = tv.DequeueReusableCell("BrandCell");
+			}
+			//tvc.Cell = item;
+
+			//WireUpForceUpdateSizeRequested(item, tvc, tv);
+
+			//tvc.TextLabel.Text = item.ToString();
+
+			//UpdateBackground(tvc, item);
+			return tvc;
+		}
+	}
+}
