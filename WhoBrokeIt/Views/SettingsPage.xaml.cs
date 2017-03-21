1bdd149f61b46d2cf7712964675e5d8fbed3fc10
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-21 17:00:27 -0600

    Send email message.

--- WhoBrokeIt/Views/SettingsPage.xaml.cs
+++ WhoBrokeIt/Views/SettingsPage.xaml.cs
@@ -18,38 +18,29 @@ namespace WhoBrokeIt.UI.Views
 
         private async void SettingCellTapped(object sender, EventArgs e)
         {
-			if (sender == DisconnectCell)
-			{
-				await DisconnectFromInstance();
-			}
-			else if (sender == ThanksToCell)
-			{
-				await Navigation.PushAsync(new LicensesPage());
-			}
-			else if (sender == ReportBugCell)
-			{
-				Device.OpenUri(new Uri("mailto:feregrino@thatcsharpguy.com"));
-			}
-			else if (sender == SourceCell)
-			{
-				Device.OpenUri(new Uri("https://github.com/messier16/WhoBrokeIt"));
-			}
-			else if (sender == IdeaCell)
-			{
-				string appReviewPage = null;
-				if (Device.OS == TargetPlatform.Android)
-				{
-					appReviewPage = "http://play.google.com/store/apps/details?id=com.facebook.katana";
-				}
-				else if (Device.OS == TargetPlatform.iOS)
-				{
-					appReviewPage = "itms-apps://itunes.apple.com/app/id353372460";
-				}
-				Device.OpenUri(new Uri(appReviewPage));
-			}
-			else if (sender == EditTokenCell)
-			{
-				await EditToken();
+            if (sender == DisconnectCell)
+            {
+                await DisconnectFromInstance();
+            }
+            else if (sender == ThanksToCell)
+            {
+                await Navigation.PushAsync(new LicensesPage());
+            }
+            else if (sender == SourceCell)
+            {
+                Device.OpenUri(new Uri("https://github.com/messier16/WhoBrokeIt"));
+            }
+            else if (sender == IdeaCell || sender == ReportBugCell)
+            {
+                var emailMessenger = Plugin.Messaging.CrossMessaging.Current.EmailMessenger;
+                if (emailMessenger.CanSendEmail)
+                {
+                    emailMessenger.SendEmail("apps@messier16.com", "About Who Broke It");
+                }
+            }
+            else if (sender == EditTokenCell)
+            {
+                await EditToken();
             }
         }
 
@@ -61,7 +52,7 @@ namespace WhoBrokeIt.UI.Views
             {
                 var accMgr = DependencyService.Get<IAccountManager>();
                 accMgr.EraseAll();
-				WhoBrokeItApp.RealCurrent.ChangeMainPage(new Views.SetKeysPage());
+                WhoBrokeItApp.RealCurrent.ChangeMainPage(new Views.SetKeysPage());
             }
         }
 
